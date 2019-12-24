bdfaddc8080a983b3d040906986d22ed6fa50559
Isabela Colucci <isabela@caosdesign.com.br>
2019-12-24 14:08:48 -0300

    Add files via upload
    Inicialização de template baseado no tutorial "Aprenda a Fazer um CRUD com Bootstrap, PHP & MySQL" de "Ruan Carvalho" publicado em "Web Dev Academy".
    
    https://webdevacademy.com.br/tutoriais/crud-bootstrap-php-mysql/

---
+++ sql/create-all-db.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS wda_crud;
+
+CREATE DATABASE wda_crud;
+
+CREATE TABLE wda_crud.customers (
+    id int(11) NOT NULL,
+    name varchar(255) NOT NULL,
+    cpf_cnpj varchar(14) NOT NULL,
+    birthdate date NOT NULL,
+    address varchar(255) NOT NULL,
+    hood varchar(100) NOT NULL,
+    zip_code int(8) NOT NULL,
+    city varchar(100) NOT NULL,
+    state varchar(100) NOT NULL,
+    phone int(13) NOT NULL,
+    mobile int(13) NOT NULL,
+    ie int(11) NOT NULL,
+    created datetime NOT NULL,
+    modified datetime NOT NULL
+);
+
+ALTER TABLE wda_crud.customers
+ADD PRIMARY KEY (id);
+
+ALTER TABLE wda_crud.customers
+MODIFY id int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
---
+++ sql/fake-customers-test.sql
@@ -0,0 +1,149 @@
+INSERT INTO `customers` (
+    `name`,
+    `cpf_cnpj`,
+    `birthdate`,
+    `address`,
+    `hood`,
+    `zip_code`,
+    `city`,
+    `state`,
+    `phone`,
+    `mobile`,
+    `ie`,
+    `created`,
+    `modified`
+) VALUES (
+    'Fulano de Tal 1',
+    '123.456.789-00',
+    '1989-01-01',
+    'Rua da Web, 123',
+    'Internet',
+    '1234568',
+    'Teste',
+    'Teste',
+    '5555555',
+    '55555555',
+    '123456',
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP
+);
+    
+INSERT INTO `customers` (
+    `name`,
+    `cpf_cnpj`,
+    `birthdate`,
+    `address`,
+    `hood`,
+    `zip_code`,
+    `city`,
+    `state`,
+    `phone`,
+    `mobile`,
+    `ie`,
+    `created`,
+    `modified`
+) VALUES (
+    'Fulano de Tal 2',
+    '123.456.789-00',
+    '1989-01-01',
+    'Rua da Web, 123',
+    'Internet',
+    '1234568',
+    'Teste',
+    'Teste',
+    '5555555',
+    '55555555',
+    '123456',
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP
+);
+
+INSERT INTO `customers` (
+    `name`,
+    `cpf_cnpj`,
+    `birthdate`,
+    `address`,
+    `hood`,
+    `zip_code`,
+    `city`,
+    `state`,
+    `phone`,
+    `mobile`,
+    `ie`,
+    `created`,
+    `modified`
+) VALUES (
+    'Fulano de Tal 3',
+    '123.456.789-00',
+    '1989-01-01',
+    'Rua da Web, 123',
+    'Internet',
+    '1234568',
+    'Teste',
+    'Teste',
+    '5555555',
+    '55555555',
+    '123456',
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP
+);
+
+INSERT INTO `customers` (
+    `name`,
+    `cpf_cnpj`,
+    `birthdate`,
+    `address`,
+    `hood`,
+    `zip_code`,
+    `city`,
+    `state`,
+    `phone`,
+    `mobile`,
+    `ie`,
+    `created`,
+    `modified`
+) VALUES (
+    'Fulano de Tal 4',
+    '123.456.789-00',
+    '1989-01-01',
+    'Rua da Web, 123',
+    'Internet',
+    '1234568',
+    'Teste',
+    'Teste',
+    '5555555',
+    '55555555',
+    '123456',
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP
+);
+
+INSERT INTO `customers` (
+    `name`,
+    `cpf_cnpj`,
+    `birthdate`,
+    `address`,
+    `hood`,
+    `zip_code`,
+    `city`,
+    `state`,
+    `phone`,
+    `mobile`,
+    `ie`,
+    `created`,
+    `modified`
+) VALUES (
+    'Fulano de Tal 5',
+    '123.456.789-00',
+    '1989-01-01',
+    'Rua da Web, 123',
+    'Internet',
+    '1234568',
+    'Teste',
+    'Teste',
+    '5555555',
+    '55555555',
+    '123456',
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP
+);
